20e996de58de4f9a7a79ac91b412512c996cfe76
Amrut Kulkarni <kuldog@gmail.com>
2022-12-20 13:30:12 -0500

    Added animation

--- 2HoppyDays/Player.gd
+++ 2HoppyDays/Player.gd
@@ -1,27 +1,28 @@
 extends KinematicBody2D
 
 var motion = Vector2(0, 0)
-const SPEED = 500
-const GRAVITY = 200
+export var move_speed = 500
+export var gravity = 200
 const UP = Vector2(0, -1)
 export var  jump_speed = 1000
 
 func _physics_process(delta):
 	apply_gravity()
 	jump()
+	animate()
 	move()
 
 func apply_gravity():
 	if not is_on_floor():
-		motion.y += GRAVITY
+		motion.y += gravity
 	else:
 		motion.y = 0
 
 func move():
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-		motion.x = -SPEED
+		motion.x = -move_speed
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
-		motion.x = SPEED
+		motion.x = move_speed
 	else:
 		motion.x = 0
 	move_and_slide(motion, UP)
@@ -29,3 +30,15 @@ func move():
 func jump():
 	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= jump_speed
+
+func animate():
+	if motion.y < 0:
+		$AnimatedSprite.play("Jump")
+	elif motion.x < 0:
+		$AnimatedSprite.play("Walk")
+		$AnimatedSprite.set_flip_h(true)
+	elif motion.x > 0:
+		$AnimatedSprite.play("Walk")
+		$AnimatedSprite.set_flip_h(false)
+	else:
+		$AnimatedSprite.play("Idle")
